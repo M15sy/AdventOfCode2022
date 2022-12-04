fbf9690fc9efb8d199651400a110a4e06000e883
M15sy <misayocurtis@hotmail.co.uk>
2022-12-04 20:15:31 +0000

    add tests

---
+++ AdventOfCode2022.Tests/ISolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2022.Core;
+
+namespace AdventOfCode2022.Tests;
+
+public class ISolutionTests
+{
+    public static IEnumerable<object[]> TestCases => new List<object[]>
+        {
+            new object[] { new Day01(), "72478", "210367" },
+            new object[] { new Day02(), "10595", "9541" },
+            new object[] { new Day03(), "8349", "2681" },
+            new object[] { new Day04(), "536", "845" },
+        };
+
+    [Theory]
+    [MemberData(nameof(TestCases))]
+    public void Should_Return_Expected(ISolution solution, string expected_1, string expected_2)
+    {
+        Assert.Equal(expected_1, solution.SolvePart1());
+        Assert.Equal(expected_2, solution.SolvePart2());
+    }
+}
--- README.md
+++ README.md
@@ -3,6 +3,5 @@
 My solutions to puzzles from the Advent of Code 2022 - http://adventofcode.com/2022
 
 ## TODO
-[] input files
 [] benchmark
 [] smalltalk
