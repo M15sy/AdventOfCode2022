a536e3c54995faa12d05df7efeef8d834eaf7a12
M15sy <misayocurtis@hotmail.co.uk>
2022-12-06 21:16:49 +0000

    swap xunit for nunit

--- AdventOfCode2022.Tests/ISolutionTests.cs
+++ AdventOfCode2022.Tests/ISolutionTests.cs
@@ -1,24 +1,22 @@
-﻿using AdventOfCode2022.Core;
-
-namespace AdventOfCode2022.Tests;
+﻿using System.Collections;
+using AdventOfCode2022.Core;
 
 public class ISolutionTests
 {
-    public static IEnumerable<object[]> TestCases => new List<object[]>
-        {
-            new object[] { new Day01(), "72478", "210367" },
-            new object[] { new Day02(), "10595", "9541" },
-            new object[] { new Day03(), "8349", "2681" },
-            new object[] { new Day04(), "536", "845" },
-            new object[] { new Day05(), "ZRLJGSCTR", "PRTTGRFPB" },
-            new object[] { new Day06(), "1198", "3120" },
-        };
-
-    [Theory]
-    [MemberData(nameof(TestCases))]
+    [TestCaseSource(nameof(ISolutionTestCases))]
     public void Should_Return_Expected(ISolution solution, string expected_1, string expected_2)
     {
-        Assert.Equal(expected_1, solution.SolvePart1());
-        Assert.Equal(expected_2, solution.SolvePart2());
+        solution.SolvePart1().Should().Be(expected_1);
+        solution.SolvePart2().Should().Be(expected_2);
+    }
+
+    private static IEnumerable ISolutionTestCases()
+    {
+        yield return new TestCaseData(new Day01(), "72478", "210367").SetName("Day01_{m}");
+        yield return new TestCaseData(new Day02(), "10595", "9541").SetName("Day02_{m}");
+        yield return new TestCaseData(new Day03(), "8349", "2681").SetName("Day03_{m}");
+        yield return new TestCaseData(new Day04(), "536", "845").SetName("Day04_{m}");
+        yield return new TestCaseData(new Day05(), "ZRLJGSCTR", "PRTTGRFPB").SetName("Day05_{m}");
+        yield return new TestCaseData(new Day06(), "1198", "3120").SetName("Day06_{m}");
     }
 }
